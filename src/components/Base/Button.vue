facfb4ad7b825bb09959c47dd8371f2b7ed8cbbb
Nikita Antonov <nikita45454@gmail.com>
2025-01-27 13:40:04 +0300

    feat: добавляет Button

---
+++ src/components/Base/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="base-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+  .base-btn {
+    cursor: pointer;
+    border-radius: 15px;
+    padding: 15px 40px;
+    font-size: 16px;
+    line-height: 1.25;
+    text-align: center;
+    background-color: var(--color-primary);
+    color: var(--color-body);
+  }
+
+  .base-btn:hover {
+    background-color: var(--color-primary-hover);
+  }
+</style>
