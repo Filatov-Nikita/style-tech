6310d5e6d0363932c853ffe0b3bd13c901e6411d
Nikita Antonov <nikita45454@gmail.com>
2025-03-11 11:26:54 +0500

    feat: добавляет SectionDevices

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -3,6 +3,9 @@
     <SectionMain />
     <SectionAbout />
     <SectionAdvantages />
+    <div class="section-devices">
+      <img class="section-devices__photo" width="2160" height="1417" src="@/assets/images/devices.jpg" alt="devices" loading="lazy">
+    </div>
   </main>
 </template>
 
@@ -11,3 +14,13 @@
   import SectionAbout from './Sections/About.vue';
   import SectionAdvantages from './Sections/Advantages.vue';
 </script>
+
+<style scoped lang="scss">
+  .section-devices {
+    padding: 80px 0;
+
+    &__photo {
+      width: 100%;
+    }
+  }
+</style>
